b845060180a4b2a5c02ea1846b617af445f6db4d
Luca Boccassi <bluca@debian.org>
2017-12-29 14:57:43 +0100

    Fix autopkgtest for non-x86_64

--- debian/tests/testsuite.sh
+++ debian/tests/testsuite.sh
@@ -13,7 +13,8 @@ cd "${dir}"
 cd testsuite
 make compile
 # build required helper not built by compile
-cc -o tools/generate_nlmsg tools/generate_nlmsg.c -I ../include/ ../lib/libnetlink.a ../lib/libutil.a /usr/lib/x86_64-linux-gnu/libmnl.a
+DEB_HOST_GNU_TYPE=$(dpkg-architecture -qDEB_HOST_GNU_TYPE)
+cc -o tools/generate_nlmsg tools/generate_nlmsg.c -I ../include/ ../lib/libnetlink.a ../lib/libutil.a "/usr/lib/${DEB_HOST_GNU_TYPE}/libmnl.a"
 
 # run tests
 make alltests

b729b2f534b7e659bcba25b5bd74b5eca97c3f2c
Luca Boccassi <bluca@debian.org>
2018-12-16 13:10:50 +0000

    autopkgtest: simplify test script by just running make check
    make check now "just works" with the latest upstream changes that have
    been backported

--- debian/tests/testsuite.sh
+++ debian/tests/testsuite.sh
@@ -6,12 +6,5 @@ dir=$(mktemp -d)
 cp -a . "${dir}"
 cd "${dir}"
 
-# build tests
-cd testsuite
-make compile
-# build required helper not built by compile
-DEB_HOST_GNU_TYPE=$(dpkg-architecture -qDEB_HOST_GNU_TYPE)
-cc -o tools/generate_nlmsg tools/generate_nlmsg.c -I ../include/ ../lib/libnetlink.a ../lib/libutil.a "/usr/lib/${DEB_HOST_GNU_TYPE}/libmnl.a"
-
-# run tests
-make alltests
+# build and run tests
+make check

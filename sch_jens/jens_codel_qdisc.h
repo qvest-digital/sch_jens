cd48d76353c69d976d5c6fcfc39754a848062ed7
mirabilos <t.glaser@tarent.de>
2021-07-27 19:33:46 +0200

    rename struct we modified

--- sch_jens/jens_codel_qdisc.h
+++ sch_jens/jens_codel_qdisc.h
@@ -51,22 +51,22 @@
  * Implemented on linux by Dave Taht and Eric Dumazet
  */
 
-/* Qdiscs using codel plugin must use codel_skb_cb in their own cb[] */
-struct codel_skb_cb {
+/* Qdiscs using codel plugin must use jens_skb_cb in their own cb[] */
+struct jens_skb_cb {
 	codel_time_t enqueue_time;
 	unsigned int mem_usage;
 	__u8 record_flag;
 };
 
-static struct codel_skb_cb *get_codel_cb(const struct sk_buff *skb)
+static struct jens_skb_cb *get_jens_cb(const struct sk_buff *skb)
 {
-	qdisc_cb_private_validate(skb, sizeof(struct codel_skb_cb));
-	return (struct codel_skb_cb *)qdisc_skb_cb(skb)->data;
+	qdisc_cb_private_validate(skb, sizeof(struct jens_skb_cb));
+	return (struct jens_skb_cb *)qdisc_skb_cb(skb)->data;
 }
 
 static codel_time_t codel_get_enqueue_time(const struct sk_buff *skb)
 {
-	return get_codel_cb(skb)->enqueue_time;
+	return get_jens_cb(skb)->enqueue_time;
 }
 
 /* from INET_ECN_set_ce (net/inet_ecn.h) */
@@ -95,7 +95,7 @@ static __u8 jens_get_ecn(struct sk_buff *skb)
 
 static void jens_set_enqueue_data(struct sk_buff *skb)
 {
-	struct codel_skb_cb *cb = get_codel_cb(skb);
+	struct jens_skb_cb *cb = get_jens_cb(skb);
 
 	cb->record_flag = jens_get_ecn(skb);
 	cb->enqueue_time = codel_get_time();
@@ -103,7 +103,7 @@ static void jens_set_enqueue_data(struct sk_buff *skb)
 
 static __u8 jens_update_record_flag(struct sk_buff *skb, __u8 data)
 {
-	struct codel_skb_cb *cb = get_codel_cb(skb);
+	struct jens_skb_cb *cb = get_jens_cb(skb);
 
 	cb->record_flag |= data;
 	return (cb->record_flag);

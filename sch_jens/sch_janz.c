a19cb33f2ae0afc77f2da4b2847db4adf21a949f
mirabilos <t.glaser@tarent.de>
2022-08-16 00:34:48 +0200

    advance bringing sch_janz to feature parity with sch_{jhtb+jens}¹
    ① as regards features actually used in the JENS

--- sch_jens/sch_janz.c
+++ sch_jens/sch_janz.c
@@ -1,5 +1,5 @@
 /*
- * Dǟ janz zesammene Kuh-Disk
+ * Dǟ janz zesammene Kwejü-Disk
  *
  * Copyright © 2022 mirabilos <t.glaser@tarent.de>
  * Licensor: Deutsche Telekom LLCTO
@@ -7,11 +7,11 @@
  * This module for the Linux kernel is published under the GPLv2.
  */
 
-#undef JENS_IP_DECODER_DEBUG
+#undef JANZ_IP_DECODER_DEBUG
 #if 1
-#define JENS_IP_DECODER_DEBUG(...)	/* nothing */
+#define JANZ_IP_DECODER_DEBUG(...)	/* nothing */
 #else
-#define JENS_IP_DECODER_DEBUG(...)	printk(__VA_ARGS__)
+#define JANZ_IP_DECODER_DEBUG(...)	printk(__VA_ARGS__)
 #endif
 
 #include <linux/version.h>
@@ -37,6 +37,33 @@
 #include <net/pkt_cls.h>
 #include "jens_codel.h"
 
+struct janz_fragcomp {
+	__u8 sip[16];				//@0    :16
+	__u8 dip[16];				//@16   :16
+	__u32 idp;				//@16   :4
+	__u8 v;					//@  +4 :1
+} __attribute__((__packed__));
+
+struct janz_fragcache {
+	struct janz_fragcomp c;			//@0
+	__u8 nexthdr;				//@ +37 :1
+	__u8 _pad[2];				//@ +38 :2
+	codel_time_t ts;			//@ +40 :4
+	__u16 sport;				//@ +44 :2
+	__u16 dport;				//@ +46 :2
+	struct janz_fragcache *next;		//@16
+};
+
+/* compile-time assertion */
+struct janz_fragcache_check {
+	int cmp[sizeof(struct janz_fragcomp) == 37 ? 1 : -1];
+	int cac[sizeof(struct janz_fragcache) == (48 + sizeof(void *)) ? 1 : -1];
+	int tot[sizeof(struct janz_fragcache) <= 64 ? 1 : -1];
+	int xip[sizeof_field(struct tc_jens_relay, xip) == 16 ? 1 : -1];
+	int x_y[offsetof(struct tc_jens_relay, yip) ==
+	    (offsetof(struct tc_jens_relay, xip) + 16) ? 1 : -1];
+};
+
 struct janz_skbfifo {
 	struct sk_buff *first;
 	struct sk_buff *last;
@@ -45,14 +72,23 @@ struct janz_skbfifo {
 /* struct janz_priv *q = qdisc_priv(sch); */
 struct janz_priv {
 	struct janz_skbfifo q[3];	/* TOS FIFOs */					//@cacheline
+	struct rchan *record_chan;	/* relay to userspace */			//@16
+#define QSZ_INTERVAL ((u64)(5UL * NSEC_PER_MSEC))
+	u64 qsz_next;			/* next time to emit queue-size */		//@  +8
 	u64 notbefore;			/* ktime_get_ns() to send next, or 0 */		//@16
 	u64 ns_pro_byte;		/* traffic shaping tgt bandwidth */		//@  +8
 	u64 handover;			/* time past next handover (or 0) */		//@16
 	codel_time_t markfree;								//@  +8
 	codel_time_t markfull;								//@  +12
+	struct janz_fragcache *fragcache_used;						//@16
+	struct janz_fragcache *fragcache_last; /* last used element */			//@  +8
+	struct janz_fragcache *fragcache_free;						//@16
+	struct janz_fragcache *fragcache_base;						//@  +8
 	struct qdisc_watchdog watchdog;	/* to schedule when traffic shaping */		//@16
 	u32 nsubbufs;									//@?
 	u32 fragcache_num;
+	codel_time_t fragcache_aged;
+	spinlock_t record_lock;		/* for record_chan */
 	u8 crediting;
 };
 
@@ -61,11 +97,13 @@ struct janz_skb {
 	/* limited to QDISC_CB_PRIV_LEN (20) bytes! */
 	codel_time_t enq_ts;		/* enqueue timestamp */			//@8   :4
 	u16 chance;			/* chance on ECN CE marking */		//@ +4 :2
-	u8 record_flag;			/* for debugfs/relayfs reporting */	//@ +6 :1
-	u8 tosbyte;			/* from IPv4/IPv6 header or faked */	//@ +7 :1
+	u16 srcport;								//@ +6 :2
+	u16 dstport;								//@8   :2
+	u8 record_flag;			/* for debugfs/relayfs reporting */	//@ +2 :1
+	u8 tosbyte;			/* from IPv4/IPv6 header or faked */	//@ +3 :1
+	u8 ipver;								//@ +4 :1
+	u8 nexthdr;								//@ +5 :1
 };
-
-static struct dentry *janz_debugfs_main;
 
 static inline struct janz_skb *
 get_janz_skb(const struct sk_buff *skb)
@@ -74,7 +112,33 @@ get_janz_skb(const struct sk_buff *skb)
 	return ((struct janz_skb *)qdisc_skb_cb(skb)->data);
 }
 
-static struct sk_buff *
+static inline void
+janz_record_write(struct tc_jens_relay *record, struct janz_priv *q)
+{
+	unsigned long flags;	/* used by spinlock macros */
+
+	spin_lock_irqsave(&q->record_lock, flags);
+	__relay_write(q->record_chan, record, sizeof(struct tc_jens_relay));
+	spin_unlock_irqrestore(&q->record_lock, flags);
+}
+
+static inline void
+janz_record_queuesz(struct Qdisc *sch, struct janz_priv *q, u64 now)
+{
+	struct tc_jens_relay r = {0};
+
+	r.ts = now;
+	r.type = TC_JENS_RELAY_QUEUESZ;
+	r.d32 = /* q->memory_usage */ 0;
+	r.e16 = sch->q.qlen > 0xFFFFU ? 0xFFFFU : sch->q.qlen;
+	r.f8 = 0;
+	janz_record_write(&r, q);
+
+	/* use of ktime_get_ns() is deliberate */
+	q->qsz_next = ktime_get_ns() + QSZ_INTERVAL;
+}
+
+static inline struct sk_buff *
 janz_getnext(struct Qdisc *sch, struct janz_priv *q, bool is_peek)
 {
 	u64 now = ktime_get_ns();
@@ -85,7 +149,8 @@ janz_getnext(struct Qdisc *sch, struct janz_priv *q, bool is_peek)
 		if (!is_peek)
 			qdisc_watchdog_schedule_range_ns(&q->watchdog,
 			    q->notbefore, NSEC_PER_MSEC);
-		return (NULL);
+		skb = NULL;
+		goto out;
 	}
 
 	/* we have reached notbefore, previous packet is fully sent */
@@ -94,7 +159,8 @@ janz_getnext(struct Qdisc *sch, struct janz_priv *q, bool is_peek)
 		/* nothing to send, start subsequent packet later */
  nothing_to_send:
 		q->crediting = 0;
-		return (NULL);
+		skb = NULL;
+		goto out;
 	}
 
 	skb = (q->q[(qid = 0)].first) ? : \
@@ -103,7 +169,7 @@ janz_getnext(struct Qdisc *sch, struct janz_priv *q, bool is_peek)
 	if (WARN(!skb, "supposed to equal !sch->q.qlen"))
 		goto nothing_to_send;
 	if (is_peek)
-		return (skb);
+		goto out;
 	if (!(q->q[qid].first = skb->next))
 		q->q[qid].last = NULL;
 	--sch->q.qlen;
@@ -115,6 +181,9 @@ janz_getnext(struct Qdisc *sch, struct janz_priv *q, bool is_peek)
 	    (q->ns_pro_byte * (u64)skb->len);
 	q->crediting = 1;
 
+ out:
+	if (!is_peek && (now >= q->qsz_next))
+		janz_record_queuesz(sch, q, now);
 	return (skb);
 }
 
@@ -177,6 +246,47 @@ janz_get_iptos(struct sk_buff *skb)
 	}
 }
 
+static inline void
+janz_analyse(struct Qdisc *sch, struct janz_priv *q,
+    struct sk_buff *skb, struct janz_skb *cb)
+{
+	//XXX fold later, plus ipver, nexthdr, srcport, dstport
+	// enq_ts done in caller
+	// chance done at dequeueing
+	// record_flag… tbd?
+	cb->tosbyte = janz_get_iptos(skb);
+}
+
+static struct dentry *
+janz_debugfs_create(const char *filename, struct dentry *parent,
+    umode_t mode, struct rchan_buf *buf, int *is_global)
+{
+	*is_global = 1;
+	return (debugfs_create_file(filename, mode, parent, buf,
+	    &relay_file_operations));
+}
+
+static int
+janz_debugfs_destroy(struct dentry *dentry)
+{
+	debugfs_remove(dentry);
+	return (0);
+}
+
+static int
+janz_subbuf_init(struct rchan_buf *buf, void *subbuf_,
+    void *prev_subbuf, size_t prev_padding)
+{
+	size_t n;
+	struct tc_jens_relay *subbuf = (struct tc_jens_relay *)subbuf_;
+	struct tc_jens_relay bufinit = { 0, TC_JENS_RELAY_PADDING };
+
+	for (n = 0; n < TC_JENS_RELAY_NRECORDS; ++n)
+		subbuf[n] = bufinit;
+
+	return (1);
+}
+
 static int
 janz_enq(struct sk_buff *skb, struct Qdisc *sch, struct sk_buff **to_free)
 {
@@ -185,9 +295,11 @@ janz_enq(struct sk_buff *skb, struct Qdisc *sch, struct sk_buff **to_free)
 	u8 qid;
 	u32 prev_backlog = sch->qstats.backlog;
 	bool overlimit;
+	u64 now;
 
-	cb->enq_ts = codel_get_time();
-	cb->tosbyte = janz_get_iptos(skb);
+	now = ktime_get_ns();
+	cb->enq_ts = codel_mktime(now);
+	janz_analyse(sch, q, skb, cb);
 
 	qid = 1;
 	if (cb->tosbyte & 0x10)
@@ -209,6 +321,9 @@ janz_enq(struct sk_buff *skb, struct Qdisc *sch, struct sk_buff **to_free)
 		q->q[qid].last = skb;
 	}
 	qdisc_qstats_backlog_inc(sch, skb);
+
+	if (now >= q->qsz_next)
+		janz_record_queuesz(sch, q, now);
 
 	if (unlikely(overlimit)) {
 		qdisc_qstats_overlimit(sch);
@@ -239,7 +354,7 @@ janz_peek(struct Qdisc *sch)
 	return (janz_getnext(sch, q, true));
 }
 
-static void
+static inline void
 janz_reset(struct Qdisc *sch)
 {
 	struct janz_priv *q = qdisc_priv(sch);
@@ -258,9 +373,6 @@ janz_reset(struct Qdisc *sch)
 	sch->qstats.overlimits = 0;
 	q->notbefore = 0;
 	q->crediting = 0;
-	//XXX qsz_next
-
-	//XXX empty fragcache
 	//XXX flush subbufs?
 }
 
@@ -274,7 +386,7 @@ static const struct nla_policy janz_nla_policy[TCA_JANZ_MAX + 1] = {
 	[TCA_JANZ_FRAGCACHE]	= { .type = NLA_U32 },
 };
 
-static int
+static inline int
 janz_chg(struct Qdisc *sch, struct nlattr *opt, struct netlink_ext_ack *extack)
 {
 	struct janz_priv *q = qdisc_priv(sch);
@@ -315,6 +427,10 @@ janz_chg(struct Qdisc *sch, struct nlattr *opt, struct netlink_ext_ack *extack)
 
 		tmp *= NSEC_PER_USEC;
 		q->handover = ktime_get_ns() + tmp;
+		/* implementation of handover */
+		q->notbefore = q->handover < q->notbefore ?
+		    q->notbefore : q->handover;
+		q->crediting = 0;
 	}
 
 	if (tb[TCA_JANZ_MARKFREE]) {
@@ -350,11 +466,20 @@ janz_chg(struct Qdisc *sch, struct nlattr *opt, struct netlink_ext_ack *extack)
 	return (0);
 }
 
+static struct dentry *janz_debugfs_main __read_mostly;
+
+static /*const*/ struct rchan_callbacks janz_debugfs_relay_hooks = {
+	.create_buf_file = janz_debugfs_create,
+	.remove_buf_file = janz_debugfs_destroy,
+	.subbuf_start = janz_subbuf_init,
+};
+
 static int
 janz_init(struct Qdisc *sch, struct nlattr *opt, struct netlink_ext_ack *extack)
 {
 	struct janz_priv *q = qdisc_priv(sch);
-	int err;
+	int err, i;
+	char name[6];
 
 	/* config values’ defaults */
 	sch->limit = 10240;
@@ -369,28 +494,51 @@ janz_init(struct Qdisc *sch, struct nlattr *opt, struct netlink_ext_ack *extack)
 	sch->q.qlen = 0;
 	janz_reset(sch);
 	qdisc_watchdog_init_clockid(&q->watchdog, sch, CLOCK_MONOTONIC);
-	//q->record_chan = NULL;
-	//spin_lock_init(&q->record_lock);
 
 	if (opt && (err = janz_chg(sch, opt, extack)))
 		goto init_fail;
 
-	if (q->nsubbufs < 4 || q->nsubbufs > 0x000FFFFF)
+	if (q->nsubbufs < 4U || q->nsubbufs > 0x000FFFFFU)
 		q->nsubbufs = 1024;
 
-	if (q->fragcache_num < 16 || q->fragcache_num > 0x00FFFFFF)
+	if (q->fragcache_num < 16U || q->fragcache_num > 0x00FFFFFFU)
 		q->fragcache_num = 1024;
 
-	//XXX subbufs, record_chan
-	//XXX fragcache
+	snprintf(name, sizeof(name), "%04X:", sch->handle >> 16);
+	q->record_chan = relay_open(name, janz_debugfs_main,
+	    TC_JENS_RELAY_SUBBUFSZ, q->nsubbufs,
+	    &janz_debugfs_relay_hooks, sch);
+	if (!q->record_chan) {
+		printk(KERN_WARNING "sch_janz: relay channel creation failed\n");
+		err = -ENOENT;
+		goto init_fail;
+	}
+	spin_lock_init(&q->record_lock);
+
+	q->fragcache_base = kvcalloc(q->fragcache_num,
+	    sizeof(struct janz_fragcache), GFP_KERNEL);
+	if (!q->fragcache_base) {
+		err = -ENOMEM;
+		goto init_fail;
+	}
+	q->fragcache_used = NULL;
+	q->fragcache_last = NULL;
+	q->fragcache_free = &(q->fragcache_base[0]);
+	for (i = 1; i < q->fragcache_num; ++i)
+		q->fragcache_base[i - 1].next = &(q->fragcache_base[i]);
+	q->fragcache_base[q->fragcache_num - 1].next = NULL;
+	q->fragcache_aged = 0;
+
+	q->qsz_next = ktime_get_ns() + QSZ_INTERVAL;
 
 	sch->flags &= ~TCQ_F_CAN_BYPASS;
 	return (0);
 
-// alloc_fail:
  init_fail:
-	//if (q->record_chan)
-	//	relay_close(q->record_chan);
+	if (q->record_chan) {
+		relay_close(q->record_chan);
+		q->record_chan = NULL;
+	}
 	return (err);
 }
 
@@ -400,7 +548,9 @@ janz_done(struct Qdisc *sch)
 	struct janz_priv *q = qdisc_priv(sch);
 
 	qdisc_watchdog_cancel(&q->watchdog);
-	//free fragcache
+	if (q->record_chan)
+		relay_close(q->record_chan);
+	kvfree(q->fragcache_base);
 }
 
 static int
@@ -452,7 +602,7 @@ janz_modinit(void)
 	int rv;
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 1, 0)
-	printk(KERN_WARNING "sch_jens: kernel too old: will misfunction for locally originating packets, see README\n");
+	printk(KERN_WARNING "sch_janz: kernel too old: will misfunction for locally originating packets, see README\n");
 #endif
 
 	if (!(janz_debugfs_main = debugfs_create_dir("sch_janz", NULL)))
@@ -460,7 +610,6 @@ janz_modinit(void)
 	else
 		rv = PTR_ERR_OR_ZERO(janz_debugfs_main);
 	if (rv) {
-		janz_debugfs_main = NULL;
 		printk(KERN_WARNING "sch_janz: debugfs initialisation error\n");
 		goto e0;
 	}
@@ -485,4 +634,4 @@ module_init(janz_modinit);
 module_exit(janz_modexit);
 MODULE_AUTHOR("Deutsche Telekom LLCTO");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("packet scheduler for JENS");
+MODULE_DESCRIPTION("bespoke packet scheduler for JENS");

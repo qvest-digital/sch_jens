b39159201f21822310e4e0b604397c1746e29d91
mirabilos <t.glaser@tarent.de>
2022-12-30 14:16:07 +0100

    add tab-separated format output to jensdmp, as an option

--- jens/jensdmp.c
+++ jens/jensdmp.c
@@ -55,6 +55,8 @@ struct tc_janz_relay rbuf[RBUF_SUBBUFSIZE < 1 ? -1 : (long)RBUF_ELEMENTLEN];
 struct cta_rbufsize { char ok[sizeof(rbuf) == RBUF_TARGETSIZE ? 1 : -1]; };
 
 static void consume(size_t);
+static void tsv_show(size_t);
+static void tsv_header(void);
 
 static volatile sig_atomic_t do_exit = 0;
 
@@ -99,9 +101,12 @@ main(int argc, char *argv[])
 	size_t n, off = 0;
 	struct pollfd pfd;
 	int pres;
+	int format = 0;
 
-	if (argc != 2)
-		errx(255, "Usage: %s /path/to/debugfs/sch_janz/nnnn:0",
+	if (argc == 3 && !strcmp(argv[1], "-t"))
+		format = 1;
+	else if (argc != 2)
+		errx(255, "Usage: %s [-t] /path/to/debugfs/sch_janz/nnnn:0",
 		    argc > 0 ? argv[0] : "jensdmp");
 
 #define setup_signal(sig) \
@@ -113,11 +118,14 @@ main(int argc, char *argv[])
 	setup_signal(SIGTERM);
 #undef setup_signal
 
-	if ((fd = open(argv[1], O_RDONLY)) == -1)
+	if ((fd = open(argv[1 + format], O_RDONLY)) == -1)
 		err(1, "open");
 
 	/* switch away from running as root */
 	drop_privs();
+
+	if (format)
+		tsv_header();
 
  loop:
 	if (do_exit)
@@ -152,7 +160,7 @@ main(int argc, char *argv[])
 
 	n = 0;
 	while ((n + sizeof(struct tc_janz_relay)) <= off) {
-		consume(n / sizeof(struct tc_janz_relay));
+		(format ? tsv_show : consume)(n / sizeof(struct tc_janz_relay));
 		n += sizeof(struct tc_janz_relay);
 		if (do_exit)
 			goto do_exit;
@@ -282,6 +290,130 @@ consume(size_t idx)
 			    (rbuf[idx].f8 & 2) ? "-" : "",
 			    (unsigned int)rbuf[idx].d32);
 		printf("/>\n");
+		break;
+	}
+}
+
+static void
+tsv_header(void)
+{
+	/*
+	 * Format:
+	 * - uppercase = number
+	 *   - with period
+	 *   - & hexadecimal
+	 * - question mark-suffixed = boolean (number 0 or 1)
+	 * - lowercase = string
+	 */
+	puts("\"p|q|w\""
+	    ",\"TS.\""
+	    ",\"OWD.|MEMBYTES|wdogscheduled?\""
+	    ",\"QDELAY.|NPKTS|NTOOEARLY\""
+	    ",\"CHANCE|handover?|N50US\""
+	    ",\"ecnin|BWLIM|N1MS\""
+	    ",\"ecnout|TSOFS.|N4MS\""
+	    ",\"bit5?|-|NLATER\""
+	    ",\"mark?|-|(THISDELAY)\""
+	    ",\"drop?|-|(&F8)\""
+	    ",\"flow|-|-\""
+	    ",\"PKTLEN|-|-\""
+	    );
+}
+
+static const char ecnidx[5][5] = {
+	"\"00\"", "\"01\"", "\"10\"", "\"11\"", "\"??\""
+};
+
+static void
+tsv_show(size_t idx)
+{
+	unsigned int t2, u1, u2, u3, u4;
+	unsigned long long ul1, t1;
+	char ipsrc[IPADDRFMTLEN], ipdst[IPADDRFMTLEN];
+	char flow[2U * IPADDRFMTLEN + 28];
+
+	ul1 = rbuf[idx].ts;
+	t1 = ul1 / 1000000000UL;
+	t2 = ul1 % 1000000000UL;
+	switch (rbuf[idx].type) {
+	case TC_JANZ_RELAY_SOJOURN:
+		if (rbuf[idx].z.zSOJOURN.ipver) {
+			ipfmt(ipsrc, rbuf[idx].x8, rbuf[idx].z.zSOJOURN.ipver);
+			ipfmt(ipdst, rbuf[idx].y8, rbuf[idx].z.zSOJOURN.ipver);
+			if (rbuf[idx].z.zSOJOURN.nexthdr == 6 ||
+			    rbuf[idx].z.zSOJOURN.nexthdr == 17)
+				snprintf(flow, sizeof(flow),
+				    "IPv%u %s [%s]:%u->[%s]:%u",
+				    (unsigned int)rbuf[idx].z.zSOJOURN.ipver,
+				    rbuf[idx].z.zSOJOURN.nexthdr == 6 ? "TCP" : "UDP",
+				    ipsrc, (unsigned int)rbuf[idx].z.zSOJOURN.sport,
+				    ipdst, (unsigned int)rbuf[idx].z.zSOJOURN.dport);
+			else
+				snprintf(flow, sizeof(flow),
+				    "IPv%u x%02X [%s]->[%s]",
+				    (unsigned int)rbuf[idx].z.zSOJOURN.ipver,
+				    (unsigned int)rbuf[idx].z.zSOJOURN.nexthdr,
+				    ipsrc, ipdst);
+		} else
+			memcpy(flow, "noIP", sizeof("noIP"));
+		ul1 = rbuf[idx].d32;
+		ul1 <<= 10;
+		u1 = ul1 / 1000000000UL;
+		u2 = ul1 % 1000000000UL;
+		ul1 = rbuf[idx].z.zSOJOURN.real_owd;
+		ul1 <<= 10;
+		u3 = ul1 / 1000000000UL;
+		u4 = ul1 % 1000000000UL;
+		printf("\"p\"\t%llu.%09u\t%u.%09u\t%u.%09u\t%04X\t%s\t%s\t%u\t%u\t%u\t\"%s\"\t%u\n",
+		    t1, t2,
+		    u3, u4,
+		    u1, u2,
+		    (unsigned int)rbuf[idx].e16,
+		    ecnidx[!(rbuf[idx].f8 & BIT(2)) ? 5 : ((unsigned int)rbuf[idx].f8 & 3U)],
+		    ecnidx[!(rbuf[idx].f8 & BIT(2)) ? 5 : (((unsigned int)rbuf[idx].f8 >> 3) & 3U)],
+		    !!(rbuf[idx].f8 & BIT(5)),
+		    !!(rbuf[idx].f8 & BIT(6)),
+		    !!(rbuf[idx].f8 & BIT(7)),
+		    flow,
+		    (unsigned int)rbuf[idx].z.zSOJOURN.psize);
+		break;
+
+	case TC_JANZ_RELAY_QUEUESZ:
+		ul1 = rbuf[idx].x64[1];
+		u2 = ul1 % 1000000000UL;
+		ul1 /= 1000000000UL;
+		printf("\"q\"\t%llu.%09u\t%u\t%u\t%u\t%llu\t%llu.%09u\n",
+		    t1, t2,
+		    (unsigned int)rbuf[idx].d32,
+		    (unsigned int)rbuf[idx].e16,
+		    !!(rbuf[idx].f8 & BIT(0)),
+		    (unsigned long long)rbuf[idx].x64[0],
+		    ul1, u2);
+		break;
+
+	case TC_JANZ_RELAY_WDOGDBG:
+		if (!(rbuf[idx].f8))
+			printf("\"w\"\t%llu.%09u\t0\t%u\t%llu\t%llu\t%llu\t%llu\n",
+			    t1, t2,
+			    (unsigned int)rbuf[idx].e16,
+			    (unsigned long long)rbuf[idx].x64[0],
+			    (unsigned long long)rbuf[idx].x64[1],
+			    (unsigned long long)rbuf[idx].y64[0],
+			    (unsigned long long)rbuf[idx].y64[1]);
+		else
+			printf("\"w\"\t%llu.%09u\t1\t%u\t%llu\t%llu\t%llu\t%llu\t%u\t%02X\n",
+			    t1, t2,
+			    (unsigned int)rbuf[idx].e16,
+			    (unsigned long long)rbuf[idx].x64[0],
+			    (unsigned long long)rbuf[idx].x64[1],
+			    (unsigned long long)rbuf[idx].y64[0],
+			    (unsigned long long)rbuf[idx].y64[1],
+			    (unsigned int)rbuf[idx].d32,
+			    (unsigned int)rbuf[idx].f8);
+		break;
+
+	default:
+		/* just ignore */
 		break;
 	}
 }
